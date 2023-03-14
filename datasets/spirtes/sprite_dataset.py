37de869adf3e12bf982aa1beede8e27be8b255e4
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-14 14:26:37 +0800

    add the basic utils to load json

---
+++ datasets/spirtes/sprite_dataset.py
@@ -0,0 +1,54 @@
+import os
+from torchvision import transforms
+from torch.utils.data import Dataset,DataLoader
+
+from PIL import Image
+from utils import *
+
+class SpriteQA(Dataset):
+    def __init__(self,split = "train",data_path = None):
+        super().__init__()
+        
+        assert split in ["train","val","test"]
+        self.split = split
+        self.root_dir = "/Users/melkor/Documents/datasets/sprites"
+        self.questions = load_json(os.path.join(self.root_dir,"sprite_qa.json"))
+        self.img_transform = transforms.Compose(
+            [transforms.ToTensor()]
+        )
+    def __len__(self): return 500#len(self.files)
+
+    def __getitem__(self,index):
+
+        image = Image.open(os.path.join(self.root_dir,self.split,"{}_{}.png".format(self.split,index)))
+        image = image.convert("RGB").resize([64,64]) 
+        image = self.img_transform(image).permute([1,2,0]) * 255
+
+        question = [q["question"] for q in self.questions[index]]
+        programs  = [p["program"] for p in self.questions[index]]
+        answers   = [a["answer"] for a in self.questions[index]]
+
+        sample = {"image":image,"question":question,"program":programs,"answer":answers}
+        return sample
+
+
+class SpriteData(Dataset):
+    def __init__(self,split = "train",data_path = None):
+        super().__init__()
+        
+        assert split in ["train","val","test"]
+        self.split = split
+        self.root_dir = "/Users/melkor/Documents/datasets/sprites"
+        self.files = os.listdir(os.path.join(self.root_dir,split))
+        self.img_transform = transforms.Compose(
+            [transforms.ToTensor()]
+        )
+    def __len__(self): return 100#len(self.files)
+
+    def __getitem__(self,index):
+        path = self.files[index]
+        image = Image.open(os.path.join(self.root_dir,self.split,"{}_{}.png".format(self.split,index)))
+        image = image.convert("RGB").resize([64,64]) 
+        image = self.img_transform(image).permute([1,2,0]) * 255
+        sample = {"image":image}
+        return sample
---
+++ utils/os.py
@@ -0,0 +1,7 @@
+import os
+import json
+
+def load_json(path):
+    with open(path,'r') as f:
+        data = json.load(f)
+        return data
