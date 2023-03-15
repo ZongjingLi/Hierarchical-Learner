6d3a4c20d7ac2ef0dcdee6da1937aff85ad4986a
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-15 08:44:44 +0800

    add the part net

--- datasets/partnet/partnet_dataset.py
+++ datasets/partnet/partnet_dataset.py
@@ -23,3 +23,29 @@ class PartNet(Dataset):
         return torch.zeros([128,128,4])
 
     def __len__(self):return 6
+
+if __name__ == "__main__":
+    import h5py
+    filename = "/Users/melkor/Documents/datasets/sem_seg_h5/Door-2/train-00.h5"
+
+    with h5py.File(filename, "r") as f:
+        # Print all root level object names (aka keys) 
+        # these can be group or dataset names 
+        print("Keys: %s" % f.keys())
+        # get first object name/key; may or may NOT be a group
+        a_group_key = list(f.keys())[0]
+
+        # get the object type for a_group_key: usually group or dataset
+        print(type(f[a_group_key])) 
+
+        # If a_group_key is a group name, 
+        # this gets the object names in the group and returns as a list
+        data = list(f[a_group_key])
+
+        # If a_group_key is a dataset name, 
+        # this gets the dataset values and returns as a list
+        data = list(f[a_group_key])
+        # preferred methods to get dataset values:
+        ds_obj = f[a_group_key]      # returns as a h5py dataset object
+        ds_arr = f[a_group_key][()]  # returns as a numpy array
+        print("datashape",f["data"].shape)
--- models/programs/symbolic.py
+++ models/programs/symbolic.py
@@ -5,3 +5,9 @@ import torch.nn.functional as F
 
 from .abstract_program import AbstractProgram
 from utils import copy_dict,apply,EPS
+
+class SymbolicProgram(AbstractProgram):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.kwargs = {}
+        self.registered = None, []
