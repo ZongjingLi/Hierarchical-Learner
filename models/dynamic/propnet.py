667f216b20efd581eb51d28b83b11ac8356a7ae9
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-29 13:01:06 +0800

    add the propnet

--- models/dynamic/propnet.py
+++ models/dynamic/propnet.py
@@ -40,3 +40,31 @@ class ParticleEncoder(nn.Module):
         """
         B,N,D = x.shape
         return self.model(x.reshape([B * N, D])).reshape([B, N, D])
+
+
+class Propagator(nn.Module):
+    def __init__(self, input_size, output_size, residual=False):
+        super(Propagator, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+        self.residual = residual
+
+        self.linear = nn.Linear(input_size, output_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x, res=None):
+        '''
+        Args:
+            x: [batch_size, n_relations/n_particles, input_size]
+        Returns:
+            [batch_size, n_relations/n_particles, output_size]
+        '''
+        B, N, D = x.size()
+        if self.residual:
+            x = self.linear(x.view(B * N, D))
+            x = self.relu(x + res.view(B * N, self.output_size))
+        else:
+            x = self.relu(self.linear(x.view(B * N, D)))
+
+        return x.view(B, N, self.output_size)
