ff0dbcccd7ad0f8b9558a5af760ee159c0e74802
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-08-05 07:25:39 +0800

    point cloud dataset

--- test.py
+++ test.py
@@ -1,38 +1,8 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+ans = 0
+for a in [2,3,4,5]:
+    ans += (1./(a*(a+1)))
 
-class LinearModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.k = nn.Parameter(torch.randn(1))
-        self.b = nn.Parameter(torch.randn(1))
+ans += (1./(6 * 8))
 
-    def forward(self, x, flag = False):
-        if flag:
-            return self.k * x + self.b
-        else:
-            return self.k * x
-
-model = LinearModel()
-
-inputs = torch.linspace(-1,1,10)
-
-gt = inputs * 5 + 3
-
-outputs = model(inputs, flag = False)
-
-optim = torch.optim.Adam(model.parameters(), lr = 1e-1)
-
-epochs = 400
-for epoch in range(epochs):
-    outputs = model(inputs, True)
-    
-    # calculate loss
-    loss = F.mse_loss(outputs, gt)
-    
-    optim.zero_grad()
-    loss.backward()
-    optim.step()
-
-print(model.k,model.b)
+print(ans)
+print(17./48)
