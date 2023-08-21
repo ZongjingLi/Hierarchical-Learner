0bd757bce0e27faec32a6e4cce82067624b5be29
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-08-21 09:36:46 +0800

    add some constraints

--- models/physics/sim/agtnet.py
+++ models/physics/sim/agtnet.py
@@ -96,7 +96,7 @@ class PropModule(nn.Module):
         self.relation_encoder = RelationEncoder(2*input_dim + relation_dim,\
                                              relation_feature_dim, relation_feature_dim)
         self.particle_propagator = Propagator(2*pfd, pfd, self.residual)
-        self.relation_propagator = Propagator(2*pfd, pfd)
+        self.relation_propagator = Propagator(2*pfd+relation_feature_dim, pfd)
         self.particle_predictor  = ParticlePredictor(pfd,output_dim,pfd)
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -128,13 +128,26 @@ class PropModule(nn.Module):
             else: pass
 
             # calculate relation effect
+            relation_effect = self.relation_propagator(
+                torch.cat([relation_encode, effect_r, effect_s], 2)
+            )
+
+            # calculate particle effect by aggregating relation effect
+            if self.batch:
+                effect_agg = Rr.bmm(relation_effect)
+            
+            # calculate particle effect
+            particle_effect = self.particle_propagator(
+                torch.cat([particle_encode, effect_agg], 2),
+                res = particle_effect
+            )
 
         pred = self.particle_predictor(particle_effect)
 
-        return 
+        return pred
 
 class AgtNet(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config, residual = False):
         super().__init__()
         
         position_dim = config.position_dim
@@ -144,10 +157,40 @@ class AgtNet(nn.Module):
         relation_fd = config.relation_feature_dim
         observation = config.observation
 
-        self.particle_encoder = ParticleEncoder()
-        self.particle_decoder = ParticlePredictor()
+        if observation == "partial":
+            batch = False
+            input_dim = state_dim
+    
+        if observation == "full":
+            batch = True
+            input_dim = state_dim + action_dim
+            self.model = PropModule(config, input_dim, position_dim, batch, residual)
 
-        self.propagator = Propagator()
+    def encode(self, data, steps):
+        state, Rr, Rs, Ra = data
+        return self.encoder(state, Rr, Rs, Ra, steps)
 
-    def forward(self, x):
-        return x
+    def decode(self, data, steps):
+        state, Rr, Rs, Ra = data
+        return self.decoder(state, Rr, Rs, Ra, steps)
+
+    def rollout(self, state, action):
+        # used only for partially observable case
+        return self.roller(torch.cat([state, action], 2))
+    
+    def to_latent(self, state, mode = "sum"):
+        if mode == "sum":
+            return torch.sum(state, 1, keepdim = True)
+        elif mode == "mean":
+            return torch.mean(state, 1, keepdim = True)
+        raise AssertionError("Unsupported Aggregation Function")
+    
+    def forward(self, data, steps, action = None):
+        # used only for fully observable case
+        config = self.config
+        state, Rr, Rs, Ra = data
+        if action is not None:
+            state = torch.cat([state, action], 2)
+        else:
+            state = state
+        return self.model(state, Rr, Rs, Ra, steps)
--- test.py
+++ test.py
@@ -5,3 +5,15 @@ from config import *
 model = AutoLearner(config)
 
 print(model)
+
+B = 2
+T = 32
+N = 11
+D = 2
+
+states = torch.randn([B,T,N,D])
+Rr = torch.ones([N,N,3])
+Rs = torch.ones([N,N,2])
+Ra = torch.ones([N,N,2])
+
+data = states, Rr, Rs, Ra
