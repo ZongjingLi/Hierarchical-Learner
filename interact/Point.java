f797c63908efe0ff02fee8b50ccaef3136028134
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-08-23 11:01:26 +0800

    compile the content

--- interact/Point.java
+++ interact/Point.java
@@ -3,7 +3,7 @@ package interact;
 public class Point {
     double x = 0.0;
     double y = 0.0;
-    String label = "namo";
+    String label = "defaultPointLabel";
 
     public Point(double x, double y) {
         this.x = x;
